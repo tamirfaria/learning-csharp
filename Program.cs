8dcda70d630ecccc9a796d40c96e18db0e7cfbca
Lucas Tamir Faria <lucastamir@gmail.com>
2023-10-11 15:15:46 -0300

    :sparkles: Implementing structures at the code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿var text = "Hello, World!";
-Console.WriteLine(text);
+﻿namespace MyApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Product mouse = new(1, "Mouse Gamer", 119.90);
+      Console.WriteLine(mouse.Id);
+      Console.WriteLine(mouse.Name);
+      Console.WriteLine(mouse.Price);
+    }
+  }
+
+  struct Product
+  {
+    public Product(int id, string name, double price)
+    {
+      Id = id;
+      Name = name;
+      Price = price;
+    }
+    public int Id;
+    public string Name;
+    public double Price;
+    public readonly double PriceInDolar(double dolar)
+    {
+      return Price * dolar;
+    }
+  }
+}
